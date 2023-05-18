5d8f7debb5dd0958ca5857e4e5e46cbad601ae19
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-05-18 16:27:58 +0530

    - updating LoginPage -

--- ChainGuardian/src/App.js
+++ ChainGuardian/src/App.js
@@ -1,7 +1,9 @@
 import './App.css'
-import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import { BrowserRouter,Route,Router,Routes } from 'react-router-dom'
 
+//pages
 import LandingPage from './pages/landingPage'
+import Login from './pages/login'
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path='/' element={<LandingPage />}/>
+          <Route path='/login' element={<Login />}/>
         </Routes>
     </BrowserRouter>
     </div>
