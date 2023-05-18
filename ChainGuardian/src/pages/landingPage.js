ab69e068b49bacc68c86f6d6363dcb157a88a843
MADHAVAN5 <kmadavan018@gmail.com>
2023-05-18 16:17:58 +0530

    UI | Hero Section

--- ChainGuardian/src/pages/landingPage.js
+++ ChainGuardian/src/pages/landingPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from "../components/navbar";
+import Hero from "../components/hero";
 
 const landingPage = () => {
   return (
+    <>
     <Navbar/>
+    <Hero/>
+    </>
+    
   )
 }
 
