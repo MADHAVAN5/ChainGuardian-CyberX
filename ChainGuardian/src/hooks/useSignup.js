0fb85067578fda209d8ec8e119a6b809edd2ebd7
Priyanka kannan <65155327+MUKILAN-2003@users.noreply.github.com>
2023-05-18 17:32:22 +0530

    - providing authContext for all child's - setup and useSignup hook adding -

---
+++ ChainGuardian/src/hooks/useSignup.js
@@ -0,0 +1,67 @@
+import { useState } from "react"
+import { firebaseAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
+import firebase from 'firebase/app'
+
+export const useSignup = () => {
+    const [error, setError] = useState(null)
+    const [isPending, setPending] = useState(false)
+    const { dispatch } = useAuthContext()
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    const signup = async (email, password, displayName) => {
+        setError(null)
+        setPending(true)
+        const abortController = new AbortController()
+
+        try {
+            const res = await firebaseAuth.createUserWithEmailAndPassword(email, password)
+            if (!res) {
+                throw new Error('could not complete signup')
+            }
+
+            await res.user.updateProfile({ displayName })
+            
+            dispatch({ type: 'LOGIN', payload: res.user })
+            
+            if (!abortController.signal.aborted) {
+                setPending(false)
+                setError(null)
+            }
+        } catch (err) {
+            if (!abortController.signal.aborted) {
+                setError(err.message)
+                setPending(false)
+            }
+        }
+        return () => {
+            abortController.abort()
+        }
+    }
+    const signUpWithGoogle = () => {
+        setError(null)
+        setPending(true)
+        const abortController = new AbortController()
+
+        firebaseAuth.signInWithPopup(provider)
+            .then((res) => {
+                dispatch({ type: 'LOGIN', payload: res.user })
+
+                if (!abortController.signal.aborted) {
+                    setPending(false)
+                    setError(null)
+                }
+
+            }).catch((err) => {
+                if (!abortController.signal.aborted) {
+                    setError(err.message)
+                    setPending(false)
+                }
+            })
+            return () => {
+                abortController.abort()
+            }
+    }
+
+    return { error, isPending, signup, signUpWithGoogle }
+}
