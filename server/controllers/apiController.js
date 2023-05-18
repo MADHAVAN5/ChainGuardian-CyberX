35fd49cf0360acda353809fdc36d2a0d818fdee5
Priyanka kannan <65155327+MUKILAN-2003@users.noreply.github.com>
2023-05-18 15:18:36 +0530

    Transaction HASH Data Integration NodeJS

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,7 +1,24 @@
 const  axios = require('axios')
 
-const testing = async (req, res) => {
-  res.json({data:'api end point'})
+const tnxDetails = async (req, res) => {
+  const chain = req.params.chain
+  const txnId = req.params.txn_id
+
+  if (!chain || !txnId) {
+    return res.status(400).json({ error: 'Missing parameters' })
+  }
+
+  const apiUrl = `https://api.blockchair.com/${chain}/dashboards/transaction/${txnId}`
+
+  axios.get(apiUrl)
+    .then(response =>{
+      const data = response.data
+      console.log(data)
+      res.json(data)
+    })
+    .catch(error => {
+      res.status(404).json({ error: error.message })
+    })
 }
 
-module.exports = { testing }
+module.exports = { tnxDetails }
