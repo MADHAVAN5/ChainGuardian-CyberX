095c9d42dfdd8d9cd950d9b404361998bb719b46
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-05-19 12:04:16 +0530

    Update apiController.js

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -44,21 +44,27 @@ const accDetails = async (req, res) => {
       finalData['feesUSD'] = data.data[address]["address"]['fees_usd']
       finalData['receivedUSD'] = data.data[address]["address"]['received_usd']
       finalData['firstReceived'] = data.data[address]["address"]['first_seen_receiving']
+      finalData['transactionCount'] = data.data[address]["address"]['transaction_count']
       finalData['lastReceived'] = data.data[address]["address"]['first_seen_spending']
       finalData['firstSend'] = data.data[address]["address"]['first_seen_spending']
       finalData['lastSend'] = data.data[address]["address"]['last_seen_spending']
 
-      finalData['transactions'] = {}
-      for (let i = 0; i < data.data[address]["calls"].length; i++) {
-        finalData['transactions'][i] = {}
-        finalData['transactions'][i]['transaction'] = data.data[address]["calls"][i].transaction_hash
-        finalData['transactions'][i]['sender'] = data.data[address]["calls"][i].sender
-        finalData['transactions'][i]['receiver'] = data.data[address]["calls"][i].recipient
-        finalData['transactions'][i]['time'] = data.data[address]["calls"][i].time
-        finalData['transactions'][i]['usd'] = data.data[address]["calls"][i].value_usd
-        finalData['transactions'][i]['transferred'] = data.data[address]["calls"][i].transferred
-        finalData['transactions'][i]['block_id'] = data.data[address]["calls"][i].block_id
-      }
+      if(chain != 'bitcoin'){
+        finalData['transactions'] = {}
+          for (let i = 0; i < data.data[address]["calls"].length; i++) {
+            finalData['transactions'][i] = {}
+            finalData['transactions'][i]['transaction'] = data.data[address]["calls"][i].transaction_hash
+            finalData['transactions'][i]['sender'] = data.data[address]["calls"][i].sender
+            finalData['transactions'][i]['receiver'] = data.data[address]["calls"][i].recipient
+            finalData['transactions'][i]['time'] = data.data[address]["calls"][i].time
+            finalData['transactions'][i]['usd'] = data.data[address]["calls"][i].value_usd
+            finalData['transactions'][i]['transferred'] = data.data[address]["calls"][i].transferred
+            finalData['transactions'][i]['block_id'] = data.data[address]["calls"][i].block_id
+          }
+        
+      }else{
+        finalData['transactions'] = data.data[address]["transactions"]
+      } 
 
       //res.json(data)
       res.json(finalData)
