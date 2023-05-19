ade239dc363e39e8513956c730a9950e637767a4
MADHAVAN5 <kmadavan018@gmail.com>
2023-05-19 11:42:08 +0530

    About and UI bug fix

--- ChainGuardian/src/pages/landingPage.js
+++ ChainGuardian/src/pages/landingPage.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Navbar from "../components/navbar";
 import Hero from "../components/hero";
+import SectionTitle from "../components/sectionTitle";
+
 
 const landingPage = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <SectionTitle
+        pretitle="About"
+        title=" Why should you use this landing page">
+        Nextly is a free landing page & marketing website template for startups
+        and indie projects. Its built with Next.js & TailwindCSS. And its
+        completely open-source.
+    </SectionTitle>
     </>
     
   )
